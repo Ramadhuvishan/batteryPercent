459ae23585d016f1769345a2e8509a246aee07c3
Ramadhuvishan <42070850+Ramadhuvishan@users.noreply.github.com>
2020-11-09 14:52:57 +0530

    Update battery.py

--- battery.py
+++ battery.py
@@ -1,5 +1,6 @@
 import psutil
 import time
+from tkinter import *
 
 battery = psutil.sensors_battery()
 plugged = battery.power_plugged
@@ -8,9 +9,23 @@ if(plugged):
     plugged = "YES"
 else:
     plugged = "NO"
-# print('Battery Percentage: '+percent+'% \nCharger Plugged:    '+plugged)
-while(True):
-    print('Battery Percentage: '+percent+'% \nCharger Plugged:    '+plugged)
-    ch = input('Enter \"EXIT\" to exit the window: ')
-    if(ch == "EXIT"):
-        break
+
+master = Tk()
+menu = Menu(master)
+master.config(menu=menu)
+filemenu = Menu(menu)
+menu.add_cascade(label='File',menu=filemenu)
+filemenu.add_command(label='Info')
+filemenu.add_separator()
+filemenu.add_command(label='Exit',command=master.quit)
+
+messageVar = Message(master, text ='Battery Percentage: '+percent+'% \nCharger Plugged:    '+plugged )
+if(percent>='80'):
+    messageVar.config(bg='lightgreen')
+elif(percent>='50' and percent<='79'):
+    messageVar.config(bg='orange')
+else:
+    messageVar.config(bg='red')
+messageVar.pack()
+master.geometry("500x200")
+mainloop()
